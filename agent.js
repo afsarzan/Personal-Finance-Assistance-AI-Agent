41efb313ef1af2711ab72ccb398950cc79a26a70
afsarzan <afsar_zan@yahoo.com>
2025-11-08 11:17:49 -0800

    Chore: setting system persona to chat

--- agent.js
+++ agent.js
@@ -5,23 +5,23 @@ const groq = new Groq({
     apiKey: process.env.GROQ_API_KEY,
 });
 
-const chatCompletion = await groq.chat.completions.create({
-  "messages": [
-    {
-      "role": "user",
-      "content": "Hi how are you?"
-    }
-  ],
-  "model": "openai/gpt-oss-20b",
-  "temperature": 1,
-  "max_completion_tokens": 8192,
-  "top_p": 1,
-  "stream": true,
-  "reasoning_effort": "medium",
-  "stop": null
-});
 
-for await (const chunk of chatCompletion) {
-  process.stdout.write(chunk.choices[0]?.delta?.content || '');
+async function callAgent(){
+  const chatCompletion = await groq.chat.completions.create({
+    "messages": [
+      {
+        role: "system",
+        content: "You are Faraz, a personal finance assistant. your task is to assist user with their expenses, balances and financial planning."
+      },
+      {
+        "role": "user",
+        "content": "Hi how much money I have?"
+      }
+    ],
+    "model": "openai/gpt-oss-20b"
+  }); 
+  console.log(JSON.stringify(chatCompletion.choices[0], null, 2));
 }
 
+
+callAgent()
